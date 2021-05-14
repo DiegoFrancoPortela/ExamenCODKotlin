d38445fe418732c54988ccedd101718a192536e0
Diego Franco Portela <dfrancoportela@danielcastelao.org>
2021-05-14 09:16:32 +0200

    Ejercicio 1 terminado

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,13 @@
 fun main(args: Array<String>) {
 
+    // ----------------------------- EJERCICIO 1 ----------------------------------
+    val claseArray = claseArray(11)
+
+    claseArray.tabla6.forEach {
+        println("6*${it / 6} = ${it}")
+    }
+
+    println("-----------")
+    // ----------------------------------------------------------------------------
+
 }
