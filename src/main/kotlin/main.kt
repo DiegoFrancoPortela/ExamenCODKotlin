13ce0c0142a1231a54903ba85a5ce14146851c03
Diego Franco Portela <dfrancoportela@danielcastelao.org>
2021-05-14 09:23:12 +0200

    PullRequest

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,6 +1,7 @@
 fun main(args: Array<String>) {
 
     // ----------------------------- EJERCICIO 1 ----------------------------------
+    // Se me olvidó hacer el PullRequest, por eso este comentario.
     val claseArray = claseArray(11)
 
     claseArray.tabla6.forEach {
